798c05d66cfe5c697ad370fd232225ab7717bd61
spikest3r <oleg0sher@outlook.com>
2025-05-03 21:09:37 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import csv
+
+result = []
+
+f = open("viewinghistory.txt",'w')
+
+total = 0
+
+print("To get viewing history go to https://www.netflix.com/settings/viewing-history and press 'Download all'")
+fileName = input("Enter CSV file name")
+with open(fileName,'r') as file:
+    csvreader = csv.reader(file)
+    fields = next(csvreader)
+    for row in csvreader:
+        data = row[0].split(":")
+        if not data[0] in result:
+            result.append(data[0])
+            print(data[0])
+            f.write(data[0] + '\n')
+            total += 1
+
+print("Total: ",total)
+
+
