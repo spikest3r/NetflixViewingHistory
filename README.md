084bca7b18a33664ec46c62b75e7a97d259508f3
spikest3r <oleg0sher@outlook.com>
2025-05-04 09:56:16 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NetflixViewingHistory
 This simple Python script will help you know all titles you have watched on Netfix.
+CSV file that Netflix gives you contains all titles you have watched, but it includes episodes of series too.
+Sometimes CSV is very messy, but this script helps to summarize everything.
+Result will be printed out in console and saved into result.txt.
--- main.py
+++ main.py
@@ -7,7 +7,9 @@ f = open("viewinghistory.txt",'w')
 total = 0
 
 print("To get viewing history go to https://www.netflix.com/settings/viewing-history and press 'Download all'")
-fileName = input("Enter CSV file name")
+fileName = input("CSV file name (leave blank for default NetflixViewingHistory.csv): ")
+if len(fileName) == 0:
+    fileName = "NetflixViewingHistory.csv"
 with open(fileName,'r') as file:
     csvreader = csv.reader(file)
     fields = next(csvreader)
